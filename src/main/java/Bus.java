83b16d6a8fa5e1da5aef02311feb5d1e64403a96
mack millar <mackmillar@gmail.com>
2021-02-09 15:11:38 +0000

    first test passed

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,4 +1,11 @@
 public class Bus {
+    private String destination;
 
+    public Bus(String destination) {
+        this.destination = destination;
+    }
+        public String getDestination(){
+            return this.destination;
 
+    }
 }
