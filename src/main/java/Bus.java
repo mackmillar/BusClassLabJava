77e565c46068baa90bbc7ca26c9f88d7c136531f
mack millar <mackmillar@gmail.com>
2021-02-09 16:02:05 +0000

    class over

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -3,10 +3,12 @@ import java.util.ArrayList;
 public class Bus {
     private String destination;
     private ArrayList<Person> capacity;
+    private Boolean capacityReached;
 
     public Bus(String destination) {
         this.destination = destination;
         this.capacity = new ArrayList<Person>();
+        this.capacityReached = false;
     }
         public String getDestination(){
             return this.destination;
@@ -15,5 +17,10 @@ public class Bus {
 
     public int passengerCount() {
         return this.capacity.size();
+    }
+
+    public void addPerson(Person person) {
+        //        will not add person if count = capacity
+        this.capacity.add(person);
     }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -6,10 +6,13 @@ import static org.junit.Assert.assertEquals;
 public class BusTest {
 
     private Bus bus;
+    private Person person;
 
     @Before
     public void before(){
     bus = new Bus("Edinburgh");
+//    Why cant this be Person passenger???
+    person = new Person("Iggy");
     }
 
     @Test
@@ -20,6 +23,19 @@ public class BusTest {
     @Test
     public void busStartsEmpty(){
         assertEquals(0, bus.passengerCount());
+    }
+
+    @Test
+    public void canAddPassenger(){
+        bus.addPerson(person);
+        assertEquals(1, bus.passengerCount());
+    }
+
+    @Test
+    public void wontBreachCapacity(){
+//        will not add person if count = capacity
+        bus.addPerson(person);
+        assertEquals(false, bus.addPerson(person));
     }
 
 }
